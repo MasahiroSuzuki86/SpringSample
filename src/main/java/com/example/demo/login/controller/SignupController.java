280b530e39697ed97f2d450380c5f21402a19ddf
MasahiroSuzuki86 <tobetobeurabemasa@gmail.com>
2022-05-29 15:05:08 +0900

    signup.htmlの修正
    個別のエラーメッセージを表示できるように修正

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +44,7 @@ public class SignupController {
 	
 	//ユーザー登録画面のPOSTコントローラー
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute SignupForm form, 
+	public String postSignUp(@ModelAttribute @Validated SignupForm form, 
 			BindingResult bindingResult, 
 			Model model) {
 		
--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -3,18 +3,44 @@ package com.example.demo.login.domain.model;
 import java.util.Date;
 import org.springframework.format.annotation.DateTimeFormat;
 import lombok.Data;
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import  org.hibernate.validator.constraints.Length;
 
 @Data
 public class SignupForm {
+	//必須入力、メールアドレス形式
+	@NotBlank
+	@Email
+	private String userId; //ユーザーID
 	
-	private String userId;
-	private String password;
-	private String userName;
+	//必須入力、長さ4から100桁まで、半角英数字のみ
+	@NotBlank
+	@Length(min=4, max=100)
+	@Pattern(regexp="^[a-zA-Z0-9]+$")
+	private String password;//パスワード
 	
+	//必須入力
+	@NotBlank
+	private String userName;//ユーザー名
+	
+	//必須入力
+	@NotNull
 	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	private Date birthday;
 	
-	private int age;
+	//値が20から100まで
+	@Min(20)
+	@Max(100)
+	private int age;//年齢
+	
+	//falseのみ可能
+	@AssertFalse
 	private boolean marriage;
 	
 	
