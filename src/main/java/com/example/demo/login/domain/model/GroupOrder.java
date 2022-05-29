b68174870d9fd5331952add289c795d409621282
MasahiroSuzuki86 <tobetobeurabemasa@gmail.com>
2022-05-30 07:00:35 +0900

    GroupOrder,ValidGroup1~3の追加
    入力チェックの順番制御機能の追加

---
+++ src/main/java/com/example/demo/login/domain/model/GroupOrder.java
@@ -0,0 +1,7 @@
+package com.example.demo.login.domain.model;
+import javax.validation.GroupSequence;
+//グループの実行」順序
+@GroupSequence({ValidGroup1.class, ValidGroup2.class, ValidGroup3.class})
+public interface GroupOrder {
+
+}
