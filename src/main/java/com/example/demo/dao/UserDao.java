39a0b8bef1082637f5d5b00067372286bfdec1c4
MasahiroSuzuki86 <tobetobeurabemasa@gmail.com>
2023-05-28 09:37:37 +0900

    DB接続テスト

---
+++ src/main/java/com/example/demo/dao/UserDao.java
@@ -0,0 +1,23 @@
+package com.example.demo.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@EnableAutoConfiguration
+@Component
+public class UserDao {
+	
+	 @Autowired
+	  private JdbcTemplate jdbc;
+
+	  public void testSelect() {
+	    List<Map<String, Object>> list = jdbc.queryForList("SELECT * FROM user");
+	    list.forEach(System.out::println);
+	  }
+
+}
--- src/main/java/com/example/demo/test/controller/testController.java
+++ src/main/java/com/example/demo/test/controller/testController.java
@@ -1,13 +1,20 @@
 package com.example.demo.test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.dao.UserDao;
 
 @Controller
 public class testController {
 	
+	@Autowired
+	private UserDao userDao;
+	
 	@GetMapping("/test")
 	public String getTest() {
+		userDao.testSelect();
 		return "test";
 	}
 }
