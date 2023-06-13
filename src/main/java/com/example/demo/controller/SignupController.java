8dd1ff4557d4e2f750ca39e4cd8864cb336274d7
MasahiroSuzuki86 <tobetobeurabemasa@gmail.com>
2023-06-13 20:53:59 +0900

    バリデーション動作確認まで
    メッセージ表示まで確認

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -3,10 +3,11 @@ package com.example.demo.controller;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,15 +44,15 @@ public class SignupController {
 	}
 	
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute @Validated SignupForm form, 
+	public String postSignUp(@ModelAttribute @Valid SignupForm form, 
 				BindingResult bindingResult, 
 				Model model) {
+		
+		System.out.println(form);
 		
 		if(bindingResult.hasErrors()) {
 			return getSignUp(form, model);
 		}
-		
-		System.out.println(form);
 		
 		return "redirect:login";
 	}
